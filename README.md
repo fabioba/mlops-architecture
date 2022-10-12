213c29b865d805ac5bc202b6b736c455c53cabd7
Fabio Barbazza <fabiobarbazza@live.it>
2022-10-12 21:10:15 +0200

    feat: mlflow in docker

--- README.md
+++ README.md
@@ -18,12 +18,25 @@ docker compose up -d
 ```
 
 ### Airflow
-
 After running docker container, visit the page: `localhost:8080`
 ![img](docs/imgs/airflow_home.png)
 
 And log into the Airflow world!
 
+Populate the `dags` folder with all the DAGS needed for the project.
+
+
+### MLFlow
+On the `docker-compose.yaml` includes the `mlflow` container in the `services` section.
+This container is responsible for running the `MLFlow server` exposed on the `localhost:600`.
+![img](docs/imgs/mlflow_home.png)
+
+Open the `example_dag.py` and set the URI of the current MLFlow server(localhost:600)
+```
+mlflow.set_tracking_uri('http://mlflow:600')
+```
+
+After updating the URI of the MLFlow server, create a new connection on `Airflow`.
 
 ### Tech Stack
 * Airflow
--- dags/example_dag.py
+++ dags/example_dag.py
@@ -1,3 +1,10 @@
+"""
+This DAG is responsible for running the sequence of steps from steps_example_dag.
+
+
+Author: Fabio Barbazza
+Date: Oct, 2022
+"""
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
@@ -8,17 +15,51 @@ logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
 
 
+
+mlflow.set_tracking_uri('http://mlflow:600')
+
+experiment = mlflow.set_experiment("Airflow_Example")
+
+
 def _task1():
-    logger.info('taks1')
-    step1_example_dag.run_workflow_step1()
+    """
+        This method is responsible for running _task1 logic
+    """
+    try:
+
+        logger.info('taks1')
+
+        step1_example_dag.run_workflow_step1()
+
+    except Exception err:
+        logger.exception(err)
+        raise err
 
 def _task2():
+    """
+        This method is responsible for running _task2 logic
+    """
+    try:
+
     logger.info('taks2')
 
+    except Exception err:
+        logger.exception(err)
+        raise err
+
 def _task3():
+    """
+        This method is responsible for running _task3 logic
+    """
+    try:
+
     logger.info('taks3')
 
+    except Exception err:
+        logger.exception(err)
+        raise err
 
+with mlflow.start_run():
 with DAG(dag_id='dag_example', start_date=datetime(2022, 1, 1), 
     schedule_interval='@daily', catchup=False) as dag:
 
