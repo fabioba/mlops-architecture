0e7e52b0446c18014c0323b297933762e33260b1
Fabio Barbazza <fabiobarbazza@live.it>
2022-10-12 20:08:13 +0200

    feat: add example_dag

---
+++ dags/example_dag.py
@@ -0,0 +1,51 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+
+import logging
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
+
+
+def _task1():
+    logger.info('taks1')
+
+def _task2():
+    logger.info('taks2')
+
+def _task3():
+    logger.info('taks3')
+
+
+with DAG(dag_id='dag_example', start_date=datetime(2022, 1, 1), 
+    schedule_interval='@daily', catchup=False) as dag:
+
+    t1 = PythonOperator(
+        task_id='t1',
+        op_kwargs=dag.default_args,
+        provide_context=True,
+        python_callable=_task1
+    )
+
+
+    t2 = PythonOperator(
+        task_id='t2',
+        op_kwargs=dag.default_args,
+        provide_context=True,
+        python_callable=_task2
+    )
+
+    t3 = PythonOperator(
+        task_id='t3',
+        op_kwargs=dag.default_args,
+        provide_context=True,
+        python_callable=_task3
+    )
+
+    t1 >> [t2, t3]
+
+
+
+    
+
